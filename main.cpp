a3ba70616f9c0152c30dde86761e9a92c493adf9
mistu <Azahar SK>
2022-05-10 21:37:52 +0530

    Googletest demo for factorial function

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Factorial.hpp"
+
+using namespace std;
+
+
+// Driver code
+int main()
+{
+    int num = 5;
+    cout << "Factorial of "
+         << num << " is " << factorial(num) << endl;
+    return 0;
+}
---
+++ test/test_calculations.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include "Factorial.hpp"
+
+TEST(test_calculations, FactorialOfZeroShouldBeOne)
+{
+    ASSERT_EQ(1, factorial(0));
+}
+
+TEST(test_calculations, FactorialOfPositiveNos)
+{
+    ASSERT_EQ(1, factorial(1));
+    ASSERT_EQ(24, factorial(4));
+    ASSERT_EQ(120, factorial(5));
+}
+
