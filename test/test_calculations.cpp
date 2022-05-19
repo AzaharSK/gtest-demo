0ab6244730e986cc73a00cf390d4fd0e29983473
AstralPlay <azahar.sk.uddin@gmail.com>
2022-05-19 15:37:38 +0530

    Update test_calculations.cpp

--- test/test_calculations.cpp
+++ test/test_calculations.cpp
@@ -6,6 +6,14 @@ TEST(test_calculations, FactorialOfZeroShouldBeOne)
     ASSERT_EQ(1, factorial(0));
 }
 
+
+TEST(test_calculations, FactorialOfNegativeShouldBeMinusOne)
+{
+    ASSERT_EQ(-1, Factorial(-5));
+    ASSERT_EQ(-1, Factorial(-1));
+    ASSERT_EQ(-1, Factorial(-45));
+}
+
 TEST(test_calculations, FactorialOfPositiveNos)
 {
     ASSERT_EQ(1, factorial(1));
